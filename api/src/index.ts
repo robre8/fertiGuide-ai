9d83357ed60d60b66d6bf661c0ff5f6528c914ba
Ronny Brenes <rjba08@gmail.com>
2026-03-11 01:29:58 -0600

    fix: use CommonJS output for Render deploy

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './schema.js';
-import { resolvers } from './resolvers.js';
+import { typeDefs } from './schema';
+import { resolvers } from './resolvers';
 import 'dotenv/config';
 
 async function main() {
